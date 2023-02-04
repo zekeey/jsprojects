a810458c5ba25c3c69cc06f965b9b2761653deff
Zekey <zekeyobrien@gmail.com>
2023-02-04 14:37:02 +1000

    Completed bin2dec script

--- bin2dec/script.js
+++ bin2dec/script.js
@@ -1,4 +1,23 @@
 const prompt = require('prompt-sync') ();
-const input = prompt("Enter binary to be converted to decimal:");
-output = parseInt(input, 10);
-console.log(output);
+
+let convertedBin = 0; 
+// get input from user
+function getInput () {
+    const INPUT = prompt("Enter binary to be converted to decimal: ");
+    return INPUT.split('');
+}
+
+// Iterate through BIN_SUBSTRINGS array using conversion formula
+function convertInput () {
+    const BIN_ARRAY = getInput();
+    for (let i = 0; i < BIN_ARRAY.length - 1;) {
+        for(let x = BIN_ARRAY.length - 1; x > 0; x--) {
+            let convertingBin = Math.round(parseInt(BIN_ARRAY[i], 2) * Math.pow(2, x));
+            convertedBin += convertingBin;
+            i++;
+        }
+    }
+    console.log(`Binary converted to decimal: ${convertedBin}`);
+}
+
+convertInput();
